c396eab612840065ed69cfbfd5a6717ec322e100
xiaoxinwan <lixinxi1010@gmail.com>
2019-03-11 11:34:02 +0800

    基本完成home页，

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,39 @@
 // pages/home/home.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-      visible: true
+      lists: [
+        { id: 1, text: "单向数据流和双向数据绑定的区别", finished: false },
+        { id: 2, text: "手写ajax", finished: false },
+        { id: 3, text: "什么是跨域，跨域的方法有几种", finished: true },
+        { id: 4, text: "promise的用法是什么，能手写一下", finished: false },
+        { id: 5, text: "let,const,vard的区别", finished: true },
+        { id: 6, text: "闭包是什么", finished: true }
+
+      ],
+      visibleConfirm: false
     },
-    confirm(event) {
-      console.log(event.detail)
+    hideConfirm() {
+      this.setData({ visibleConfirm: false})
     },
-    cancel(event) {
-      this.setData({ visible: false})
+    showConfirm() {
+      this.setData({ visibleConfirm: true })
     },
+    createConfirm(event) {
+      let content = event.detail
+      if(content) {
+        let todo = [{id: this.data.lists.length+1, text: content, finished: false}]
+        this.data.lists = todo.concat(this.data.lists)
+        this.setData({ lists: this.data.lists})
+        this.hideConfirm()
+      }
+    },
+    completeTodo(event) {
+      let index = event.currentTarget.dataset.index
+      this.data.lists[index].finished = true
+      this.setData({ lists: this.data.lists})
+    },
+
+
     /**
      * 生命周期函数--监听页面加载
      */
