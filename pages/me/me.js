1c77ce2142b337f7506a7d4589855acfe7177e7b
xiaoxinwan <lixinxi1010@gmail.com>
2019-03-16 19:06:14 +0800

    完成个人页，等待数据接口

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,66 +1,32 @@
-// pages/me/me.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tab: 'tomato',
+    lists: {
+      "本周一": [
+        { time: "12:00", text: "今天吃饭了吗", id: "1-1" },
+        { time: "13:00", text: "今天吃饭了吗", id: "1-2" },
+        { time: "14:00", text: "今天吃饭了吗", id: "1-3" }
+      ],
+      "本周二": [
+        { time: "06:00", text: "今天吃饭了吗", id: "2-1" },
+        { time: "08:00", text: "今天吃饭了吗", id: "2-2" }
+      ],
+      "本周三": [
+        { time: "18:00", text: "今天吃饭了吗", id: "3-1" },
+        { time: "20:00", text: "今天吃饭了吗", id: "3-2" },
+        { time: "22:00", text: "今天吃饭了吗", id: "3-3" }
+      ],
+      "本周四": [
+        { time: "06:00", text: "今天吃饭了吗", id: "4-1" },
+        { time: "08:00", text: "今天吃饭了吗", id: "4-2" }
+      ],
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  changeTab(event) {
+    let name = event.target.dataset.name
+    this.setData({tab: name})
+  },
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
